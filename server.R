40be49eee7a2b13a92d7013bc262c8cad3ae4a7b
florianm <florian.mayer@dpaw.wa.gov.au>
2015-03-14 13:51:41 +0800

    loads csv from URL and displays cols in dropdowns

--- server.R
+++ server.R
@@ -2,5 +2,40 @@ library(shiny)
 
 shinyServer(function(input, output) {
 
+  data <- reactive({
+      infile <- input$csv_url
+      if (is.null(infile)) { return(NULL) }
+      read.table(input$csv_url, sep=",", header=T, stringsAsFactors=T)
+    })
 
+  output$xcol <- renderUI({
+    df <-data()
+    if (is.null(df)) return(NULL)
+    items=names(df)
+    names(items)=items
+    selectInput("xcol", "Select X Variable", items)
+  })
+
+  output$ycol <- renderUI({
+    df <-data()
+    if (is.null(df)) return(NULL)
+    items=names(df)
+    names(items)=items
+    selectInput("ycol", "Select Y Variable", items)
+  })
+
+  output$gcol <- renderUI({
+    df <-data()
+    if (is.null(df)) return(NULL)
+    items=names(df)
+    names(items)=items
+    selectInput("gcol", "Select Group Variable", items)
+  })
+
+  output$data <- data()
+
+
+  output$table <- renderDataTable({
+    data()
+    })
 })
--- ui.R
+++ ui.R
@@ -8,15 +8,21 @@ shinyUI(
       fluidRow(
         column(3,
                wellPanel(
-                 h3("Load Data"),
-                 selectizeInput('ckan_url', 'Select catalogue', choices = ckan_instances),
-                 textInput("api_key", "Paste your CKAN API key"),
+                 h4("Load Data"),
                  textInput("csv_url", "Paste CSV data resource URL"),
+                 uiOutput("xcol"),
+                 uiOutput("ycol"),
+                 uiOutput("gcol")
+               ), # /wellPanel
+               wellPanel(
+                 h4("Save Figure"),
+                 textInput("api_key", "Paste your CKAN API key"),
                  textInput("pdf_url", "Paste PDF graph resource URL")
                ) # /wellPanel
         ), # /col3
         column(9,
-               p("Inspect data selection")
+               p("Inspect data selection"),
+               dataTableOutput("table")
         ) # /col9
       ) # /fluidRow
     ) # /tabPanel
